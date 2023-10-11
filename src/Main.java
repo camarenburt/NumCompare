86155a4ad206a1076dd7b2e471da5b4bae9254d1
621352 <camarenburt>
2023-10-11 09:05:32 -0400

    edgf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int num1;
+        int num2;
+
+        System.out.println("Please choose a number.");
+        num1 = scan.nextInt();
+
+        System.out.println("Please choose a number.");
+        num2 = scan.nextInt();
+
+        if (num1 == num2) {
+            System.out.println(num1 + " is equal to " + num2 + ".");
+        }
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2 + ".");
+        }
+        if (num1 < num2) {
+            System.out.println(num1 + " is less than " + num2 + ".");
+        }
     }
 }
